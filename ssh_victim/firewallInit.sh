78a9c3e44d2a7ef906ed5b8c73d672c0a0eabd53
MacroMan5 <119378668+MacroMan5@users.noreply.github.com>
2023-05-18 14:24:27 -0400

    Add Firewall Init script

---
+++ ssh_victim/firewallInit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Activer UFW
+ufw enable
+
+# Réinitialiser toutes les règles de pare-feu
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
